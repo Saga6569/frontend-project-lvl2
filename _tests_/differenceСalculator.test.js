acba5390461fb71201ffc3b56de1574d6ee0108a
saga6569 <aristov6569@gmail.com>
2020-09-26 19:54:58 +0200

    step 5.1

--- _tests_/differenceСalculator.test.js
+++ _tests_/differenceСalculator.test.js
@@ -1,15 +1,15 @@
 import { test, expect } from '@jest/globals';
-import differenceСalculator from '../bin/differenceСalculator.js'; 
+import { differenceCalculator } from '../src/utils.js'; 
 
 const data1 = {
     "timeout": 50
 };
 const data2 = {
     "timeout": 20
-}
+};
 
-const result = { '+timeout': 50, '-timeout': 20 }
+const result = { '+ timeout': 50, '- timeout': 20 };
 
 test('test 1', () => {
-  expect(differenceСalculator(data1, data2)).toEqual(result);
+  expect(differenceCalculator(data1, data2)).toEqual(result);
 });
