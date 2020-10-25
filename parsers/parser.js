e4611288abc24737542f505dd3e4a99f9aaabbb3
saga6569 <aristov6569@gmail.com>
2020-10-25 17:16:14 +0200

    step comp 2.8

--- parsers/parser.js
+++ parsers/parser.js
@@ -2,16 +2,17 @@ import _ from 'lodash';
 
 const perserJs = (objct) => {
   const keys = Object.keys(objct);
-  return keys.reduce((acc, key) => {
+  const result = keys.reduce((acc, key) => {
     if (_.isObject(objct[key])) {
       // eslint-disable-next-line no-param-reassign
-      acc += ` ${key} :  ${perserJs(objct[key])} \n `;
-    } else {
+      acc += `${key} : ${perserJs(objct[key])} \n`;
+    } else if (!_.isObject(objct[key])) {
       // eslint-disable-next-line no-param-reassign
-      acc += ` ${key} : ${objct[key]} \n`;
+      acc += `${key} : ${objct[key]} \n`;
     }
     return acc;
-  }, '');
+  }, '{ \n');
+  return `${result}}`;
 };
 
 export default perserJs;
