f9a3169b5aa1c3866537c321999fe189d4bbcfce
saga6569 <aristov6569@gmail.com>
2020-10-26 18:05:48 +0200

    refarting 1.1

--- parsers/parser.js
+++ parsers/parser.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-param-reassign */
 import _ from 'lodash';
 
 const perserJs = (objct) => {
-  const keys = Object.keys(objct);
-  const result = keys.reduce((acc, key) => {
-    if (_.isObject(objct[key])) {
-      // eslint-disable-next-line no-param-reassign
-      acc += `${key} : ${perserJs(objct[key])} \n`;
-    } else if (!_.isObject(objct[key])) {
-      // eslint-disable-next-line no-param-reassign
-      acc += `${key} : ${objct[key]} \n`;
-    }
-    return acc;
-  }, '{ \n');
-  return `${result}}`;
+  const iter = (objctt, dep) => {
+    const speas = (count = 1) => '    '.repeat(count);
+    const keys = Object.keys(objctt);
+    const result = keys.reduce((acc, key) => {
+      if (_.isObject(objctt[key])) {
+        acc += `${speas(dep)} ${key}: ${iter(objctt[key], dep + 1)} \n`;
+      } else {
+        acc += `${speas(dep)} ${key}: ${objctt[key]} \n`;
+      }
+      return acc;
+    }, '{ \n');
+    return `${result} ${speas(dep)} }`;
+  };
+  return iter(objct, 0);
 };
 
 export default perserJs;
