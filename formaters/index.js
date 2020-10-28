641364fbead3ce6e10bd3e49981c4feaa9a0cfdb
saga6569 <aristov6569@gmail.com>
2020-10-28 15:21:03 +0200

    ref 1.5

--- formaters/index.js
+++ formaters/index.js
@@ -1,15 +1,18 @@
 import _ from 'lodash';
 import planCalculator from './plain.js';
 import differenceCalculator from './json.js';
+import perserJs from '../parsers/parser.js';
 
-export const diffCalculator = {
+const diffCalculator = {
   plain: planCalculator,
   json: differenceCalculator,
 };
 
-export const getDiffCalculator = (fail1, fail2, from = 'json') => {
+const getDiffCalculator = (fail1, fail2, from) => {
   if (!_.has(diffCalculator, from)) {
-    console.log('что то  пошло не так');
+    return perserJs(differenceCalculator(fail1, fail2));
   }
   return diffCalculator[from](fail1, fail2);
 };
+
+export default getDiffCalculator;
--- formaters/json.js
+++ formaters/json.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 import { keyBattery } from '../src/utils.js';
 
 const differenceCalculator = (jsFail1, jsFail2) => {
-  const allKey = keyBattery(jsFail1, jsFail2).sort();
-  return allKey.reduce((acc, key) => {
+  const arrKeyfile = keyBattery(jsFail1, jsFail2).sort();
+  return arrKeyfile.reduce((acc, key) => {
     if (_.isObject(jsFail1[key]) && _.isObject(jsFail2[key])) {
       acc[`  ${key}`] = { ...differenceCalculator(jsFail1[key], jsFail2[key]) };
     } else if (_.has(jsFail1, key) && _.has(jsFail2, key) && jsFail1[key] !== jsFail2[key]) {
